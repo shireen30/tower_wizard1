8cb1a9962f3c7b3da08daab7869f073c98e86f26
shireen30 <70129608+shireen30@users.noreply.github.com>
2021-11-04 22:55:08 +0530

    Update sketch.js
    Added fireball and magic doors

--- sketch.js
+++ sketch.js
@@ -1,32 +1,46 @@
+
+
 var canvas;
 var backgroundImage;
-var knightImgL, knightImgR, towerImg
+var knightImgL, knightImgR, towerImg, platformImg, fireballImg
 var knight, tower
-var platform1, platform2, platform3
-
+var platform1
+var platformGroup
+var magicdoorImg;
+var magicdoor;
+var magicdoorGroup;
 function preload() {
 knightImgL = loadImage("knight.png");
 knightImgR = loadImage("knight2.png")
 towerImg = loadImage("tower.jpg");
+platformImg = loadImage("platform.png")
+fireballImg = loadImage("fireball.png")
+magicdoorImg=loadImage('magicdoor.png');
 } 
+
+
 
 function setup() {
   canvas = createCanvas(windowWidth, windowHeight);
 tower = createSprite(windowWidth/2,windowHeight/2,50,50)
 tower.addImage(towerImg);
+tower.scale = 1.4
 tower.velocityY=1;
-knight = createSprite(windowWidth - 350,windowHeight - 110,50,50)
+knight = createSprite(windowWidth/2,windowHeight/2 ,50,50)
 knight.addImage(knightImgR)
 knight.addImage(knightImgL)
 knight.scale = 0.5
-platform1 = createSprite(windowWidth - 400,windowHeight - 20,250,20)
-//platform1.debug=true;
+platform1 = createSprite(windowWidth/2,windowHeight/2 + 50,250,20)
+platform1.addImage(platformImg)
+platform1.setCollider("rectangle",0,0,300,50)
+platform1.lifetime = 600
+platform1.debug=false;
 knight.debug=false;
 knight.setCollider("rectangle",0,0,200,350)
-platform2 = createSprite(windowWidth/2,windowHeight/2 + 150,200,20)
-platform3 = createSprite(windowWidth/2 - 370,windowHeight/2,250,20)
-
+platformGroup = new Group()
+magicdoorGroup=new Group();
 }
+
 
 function draw() {
  background("black");
@@ -45,18 +59,77 @@ if(keyIsDown(RIGHT_ARROW)){
   knight.position.x+=5
   knight.addImage(knightImgR)
 }
+//gravity
 knight.velocityY+=0.4
 
 if(keyDown("space"))
 {
   console.log("space")
-  knight.velocityY=-7
+  knight.velocityY=-14
+}
+
+if(keyWentDown(UP_ARROW)){
+  knight.velocityY=-5
 }
 
 knight.collide(platform1)
-knight.collide(platform2)
-knight.collide(platform3)
-//gravity
+knight.collide(platformGroup)
+spawnPlatforms();
+spawnFireballs();
+
+spawnDoors();
+
   drawSprites()
 }
 
+function spawnPlatforms(){
+  var platform
+  
+  if(frameCount%240 == 0){
+    var rand = Math.round(random(1,3))
+    console.log(rand)
+    if(rand == 1){
+      platform = createSprite(200,windowHeight-windowHeight+10,200,20)
+    }
+    else if(rand == 2){
+      platform = createSprite(windowWidth/2,windowHeight-windowHeight+10,200,20)
+    }
+    else{
+      platform = createSprite(windowWidth-200,windowHeight-windowHeight+10,200,20)
+
+    }
+    
+    platform.velocityY = 1
+    platform.addImage(platformImg);
+    platform.debug = false;
+    platform.scale = 0.9
+    platform.setCollider("rectangle",0,0,300,50)
+    platform.lifetime = 750
+    platformGroup.add(platform);
+
+  }
+}
+
+function spawnDoors(){
+  if(frameCount%300==0){
+    magicdoor=createSprite(Math.round(random(200,windowWidth-250)),100);
+    magicdoor.addImage(magicdoorImg);
+    magicdoor.velocityY=1;
+    magicdoor.scale=0.7;
+    magicdoor.lifetime=300;
+    magicdoorGroup.add(magicdoor)
+    knight.depth=magicdoor.depth+1;
+
+  }
+
+}
+function spawnFireballs(){
+  var fireball
+
+  if(frameCount%400 == 0){
+    fireball = createSprite(Math.round(random(200,windowWidth-250)),windowHeight-windowHeight+10);
+    fireball.addImage(fireballImg);
+    fireball.scale = 0.5
+    fireball.velocityY = 15
+  }
+}
